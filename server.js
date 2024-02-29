fc8f15b40a88971977ea2127133a69f3018bf1b7
Rahul Tiwari <gyanexe@gmail.com>
2024-02-29 12:57:45 +0530

    data

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 var morgan = require("morgan");
 const connectDB = require("./config/db");
+const colors = require("colors");
 //Load env vars
 dotenv.config({ path: "./config/config.env" });
 
@@ -20,11 +21,13 @@ app.use("/api/v1/bootcamps", bootcamps);
 
 const port = process.env.PORT || 5000;
 const server = app.listen(port, () => {
-  console.log(`server running in ${process.env.NODE_ENV} mode on ${port}`);
+  console.log(
+    `server running in ${process.env.NODE_ENV} mode on ${port}`.yellow.bold
+  );
 });
 
 //handle unhandled promise rejection
 process.on("unhandledRejection", (err, promise) => {
-  console.log(`Error: ${err.message}`);
+  console.log(`Error: ${err.message}`.red);
   server.close(() => process.exit(1));
 });
